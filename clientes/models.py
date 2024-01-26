bb5b40527f69875b25e65e366e30dd2d475b2bc9
Gustavo Tona <gustavopicolitona@gmail.com>
2024-01-26 14:37:52 -0300

    update cliente app - finished

---
+++ clientes/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model): #classe cliente, precisamos especificar o que tem dentro
+    primeiro_nome = models.CharField(
+        max_length=50, 
+        null=False, 
+        blank=False
+        )
+    sobrenome = models.CharField(
+        max_length=50, 
+        null=False, 
+        blank=False
+        )
+    
+    nascimento = models.CharField(
+        max_length=8, 
+        null=False, 
+        blank=False
+        )
+
+    cpf = models.CharField(
+        max_length=11, 
+        null=False, 
+        blank=False
+        )
+    
+    telefone = models.CharField(
+        max_length=9, 
+        null=False, 
+        blank=False
+        )
+    
+    endereco = models.CharField(
+        max_length=50, 
+        null=False, 
+        blank=False
+        )
+
+    registrado_em = models.DateField(
+        auto_now_add=True,
+        null=False,
+        blank=False
+        )
+    
---
+++ clientes/views.py
@@ -0,0 +1,24 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView #importar o list 
+
+from django.urls import reverse_lazy #puxar para quando concluir o create voltar para a pagina list automatico
+from .models import Cliente
+# Create your views here.
+
+#Essa funcão de cima mudou poara essa de baixo, essa de baixo é mais moderna
+
+class ClienteListView(ListView):
+    model = Cliente #Por padrão a list view procura um template, mesmo nome app e _list 
+
+class ClienteCreateView(CreateView): #quando usar o Createview precisa setar o modelo fields
+    model = Cliente
+    fields = ["primeiro_nome", "sobrenome", "nascimento", "cpf", "telefone", "endereco"] 
+    success_url = reverse_lazy ("cliente_list")
+
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    fields = ["primeiro_nome", "sobrenome", "nascimento", "cpf", "telefone", "endereco"] 
+    success_url = reverse_lazy ("cliente_list")
+
+class ClienteDeleteView(DeleteView):
+    model = Cliente
+    success_url = reverse_lazy ("cliente_list")
