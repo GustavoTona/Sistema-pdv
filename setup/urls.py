9f485108921185c8ab74eedcb2bd918b0fdc6319
Gustavo Tona <gustavopicolitona@gmail.com>
2024-01-30 11:18:06 -0300

    setup update

--- setup/urls.py
+++ setup/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path, include
+from vendas.views import VendaListView, ClienteDados
 from clientes.views import ClienteListView, ClienteCreateView, ClienteUpdateView, ClienteDeleteView
 from produtos.views import ProdutoListView, ProdutoCreateView, ProdutoUpdateView, ProdutoDeleteView, home
 
@@ -29,7 +31,9 @@ urlpatterns = [
     path("clientes/", ClienteListView.as_view(), name="cliente_list"), 
     path("create_cliente", ClienteCreateView.as_view(), name="cliente_create"),
     path("update_cliente/<int:pk>", ClienteUpdateView.as_view(), name="cliente_update"),
-    path("delete_cliente/<int:pk>", ClienteDeleteView.as_view(), name="cliente_delete")
+    path("delete_cliente/<int:pk>", ClienteDeleteView.as_view(), name="cliente_delete"),
+    path("vendas", VendaListView.as_view(), name="venda_list"),
+    path("create_venda/", ClienteDados, name="venda_create" )
 ]
 
     # path("clientes/", include('clientes.url')) #include para chamar o app clientes, import include
