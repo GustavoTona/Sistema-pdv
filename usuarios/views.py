fd20706b8d60e8c946d658a39768901dd137899a
Gustavo Dev <gustavopicolitona@gmail.com>
2024-02-08 13:07:09 -0300

    Usuarios new feature / create and login

---
+++ usuarios/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cadastro (request):
+    return render (request, "usuarios/cadastro.html")
+
+
+def login(request):
+    return render (request, 'usuarios/login.html')
