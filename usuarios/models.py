13e6b62a42204d62a1741d1afcae33778f569496
Gustavo Dev <gustavopicolitona@gmail.com>
2024-02-19 18:47:34 -0300

    Update models.py

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from django import forms
+from clientes.models import Cliente
+from produtos.models import Produto
+
+class ItemForm(forms.Form):
+    item = forms.ModelChoiceField(queryset=Cliente.objects.all())
+    produto = forms.ModelChoiceField(queryset=Produto.objects.all())
+    preco = forms.IntegerField()
+    salvar = forms.BooleanField(widget=forms.HiddenInput(), required=False)
